ee29a14f5400d202258bfb46ead51fdfca5f0e89
GSpence <spencegarett@gmail.com>
2014-03-27 15:11:24 -0400

    Steps 4-8

--- people.rb
+++ people.rb
@@ -4,16 +4,39 @@ class Person
 		@name = name
 	end
 
+#Step 5
+  	def greeting
+    	puts "Hello, my name is #{@name}"
+  	end
+end
+
 
 #Steps 1, 2, 3
-class Student
+class Student < Person
+
 	def learn
-		"I get it!"
+		puts "I get it!"
 	end
 end
 
-class Instructor
+class Instructor < Person
+
 	def teach
-		"Everything in Ruby is an Object."
+		puts "Everything in Ruby is an Object."
 	end
 end
+
+#Step 6
+person1 = Instructor.new("Chris")
+person1.greeting
+
+#Step 7
+person2 = Student.new("Cristina")
+person2.greeting
+
+#Step 8
+person1.teach
+person2.learn
+
+person2.teach
+#This failed because "teach" is only defined under the "Instructor" class, not the parent "Person" class.
